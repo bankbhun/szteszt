e0da73872b6757a1e0a12ad261c1c847ca87cfa7
Bánkúti Barnabás <bbbbarnika@gmail.com>
2023-12-18 12:34:21 +0100

    legnagyobb

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let bevittErtek
 const szamok = []
-let legkisebbSzam = 0
+let legnagyobbSzam = 0
 document.getElementById(hozGomb).addEventListener("click", () => {
     bevittErtek = document.getElementById(bevitel).value
     szamok.push(bevittErtek)
     szamok.forEach(element => {
-        if(element < legkisebbSzam)
-        legkisebbSzam = element
+        if(element > legnagyobbSzam)
+        legnagyobbSzam = element
     });
     document.getElementById(kiiras).innerHTML(legkisebbSzam)
 })
