54f67d87473de8d2ab4994292e7e733461683fd0
Bánkúti Barnabás <bbbbarnika@gmail.com>
2023-12-18 12:27:04 +0100

    novekvo

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 let bevittErtek
 const szamok = []
+const novekvo = []
+let elozoSzam = 0
 document.getElementById(hozGomb).addEventListener("click", () => {
     bevittErtek = document.getElementById(bevitel).value
     szamok.push(bevittErtek)
-    document.getElementById(kiiras).innerHTML(szamok)
+    szamok.forEach(element => {
+        if(element => elozoSzam)
+        elozoSzam = element
+        novekvo.push(element)
+    });
+    document.getElementById(kiiras).innerHTML(novekvo)
 })
